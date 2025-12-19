9a367bcae1853b1db01d2cbd11d676dd801934e3
widyaa <widyadwiaryani9@gmail.com>
2025-12-19 10:38:45 +0700

    Add model training and tuning with MLflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# LOAD DATA
+def load_data(path):
+    return pd.read_csv(path)
+
+# TRAIN + TUNING + MLFLOW
+def train_with_tuning(df):
+
+    # MLFLOW CONFIG (AMAN UNTUK CI)
+    mlflow.set_experiment("Widya-Experiment-Tuning")
+
+    # TARGET
+    y = (df["Quantity"] > df["Quantity"].median()).astype(int)
+
+    # FEATURE SELECTION
+    drop_cols = [
+        "Invoice",
+        "StockCode",
+        "Description",
+        "InvoiceDate",
+        "CustomerID"
+    ]
+
+    X = df.drop(columns=[c for c in drop_cols if c in df.columns])
+    X = X.select_dtypes(include=["int64", "float64"])
+
+    # SPLIT
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    param_grid = [
+        {"n_estimators": 50,  "max_depth": None},
+        {"n_estimators": 100, "max_depth": None},
+    ]
+
+    for params in param_grid:
+        with mlflow.start_run():
+            model = RandomForestClassifier(
+                n_estimators=params["n_estimators"],
+                max_depth=params["max_depth"],
+                random_state=42
+            )
+
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+
+            mlflow.log_param("n_estimators", params["n_estimators"])
+            mlflow.log_param("max_depth", params["max_depth"])
+
+            mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+            mlflow.log_metric("precision", precision_score(y_test, y_pred))
+            mlflow.log_metric("recall", recall_score(y_test, y_pred))
+            mlflow.log_metric("f1_score", f1_score(y_test, y_pred))
+
+            mlflow.sklearn.log_model(model, "sklearn-model")
+
+            print("Run selesai:", params)
+
+# MAIN
+if __name__ == "__main__":
+    df = load_data("processed_transaction_dataset_1000.csv")
+    train_with_tuning(df)
